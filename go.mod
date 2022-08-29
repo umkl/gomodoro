067c2276f4a96edfecfd37797376e261ae5366df
Michael Ungar <michael.ungar03@gmail.com>
2022-08-29 20:47:17 +0200

    feat: working pomodoro

--- go.mod
+++ go.mod
@@ -2,6 +2,12 @@ module gomodoro
 
 go 1.19
 
-require github.com/thatisuday/commando v1.0.4
+require (
+	github.com/thatisuday/commando v1.0.4
+)
 
-require github.com/thatisuday/clapper v1.0.10 // indirect
+require (
+	github.com/cocoonlife/goalsa v0.0.0-20211008122401-47ec570866e3 // indirect
+	github.com/cryptix/wav v0.0.0-20180415113528-8bdace674401 // indirect
+	github.com/thatisuday/clapper v1.0.10 // indirect
+)
--- main.go
+++ main.go
@@ -11,51 +11,26 @@ import (
 func main() {
 	commando.
 		SetExecutableName("gomo").
-		SetVersion("1.0.0").
+		SetVersion("0.0.0").
 		SetDescription("Pomodoro timer for your terminal")
 
 	commando.
 		Register("time").
-		AddArgument("time", "Time in minutes", "20").
+		AddArgument("time", "Time in minutes", "5").
 		SetDescription("Starts the timer").
 		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
-			fmt.Println("Starting timer for", args["time"].Value, " minutes")
+			fmt.Println("Starting timer for", args["time"].Value, "minutes")
 			inputTimeParsed, _ := strconv.Atoi(args["time"].Value)
-			pomodoroDuration := time.Duration(inputTimeParsed) * time.Minute
+			pomodoroDuration := time.Duration(inputTimeParsed) * time.Second
 			pomodoroStartTime := time.Now()
 			pomodoroEndTime := pomodoroStartTime.Add(pomodoroDuration)
-
-			for time.Now() != pomodoroEndTime {
+			for !time.Now().After(pomodoroEndTime) {
 				time.Sleep(time.Second)
 				secondsSinceStart := time.Now().Sub(pomodoroStartTime).Round(time.Second)
 				fmt.Printf("\r %v", secondsSinceStart)
 			}
 			fmt.Println("\nPomodoro finished")
+
 		})
 	commando.Parse(nil)
 }
-
-// func main() {
-// 	// Parse command-line arguments
-// 	var opts options
-// 	args, err := flags.ParseArgs(&opts, os.Args[1:])
-// 	if err != nil {
-// 		os.Exit(1)
-// 	}
-
-// 	// Convert to internal config
-// 	cfg := config.New()
-// 	cfg.Verbose = opts.Verbose
-//   	// more taking command line options and putting them into a config struct.
-
-// 	if opts.Pass {
-// 		// ask the user for a password
-// 	}
-
-// 	// Run the App
-// 	err = app.Run(cfg)
-// 	if err != nil {
-// 		// do stuff
-// 		os.Exit(1)
-// 	}
-// }
